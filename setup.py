24f0b017e5761503fd1a637460003ebffce91598
Tjaart van der Walt <git@tjaart.co.za>
2018-09-07 15:29:06 -0700

    Get PYPI to render Markdown

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@
 
 from setuptools import find_packages, setup
 
-setup(setup_requires=['pbr', 'setuptools-markdown'],
-      pbr=True,
-      packages=find_packages('timew'),
-      )
+setup(
+    setup_requires=['pbr', 'setuptools-markdown'],
+    pbr=True,
+    packages=find_packages('timew'),
+    long_description_markdown_filename='README.md'
+)
