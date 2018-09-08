ad7477d5c377cede404668d2a53ddfba69e11cc1
Tjaart van der Walt <git@tjaart.org>
2018-09-07 20:23:53 -0700

    Remove PBR dependency

--- setup.py
+++ setup.py
@@ -3,8 +3,26 @@
 from setuptools import find_packages, setup
 
 setup(
-    setup_requires=['pbr', 'setuptools-markdown'],
-    pbr=True,
+    setup_requires=['setuptools-markdown', 'setuptools-scm'],
+    name='timew',
+    use_scm_version=True,
+    author='Tjaart van der Walt',
+    author_email='tjaart@tjaart.org',
+    summary='Python bindings for your timewarrior database',
+    long_description_markdown_filename='README.md',
+    project_urls={
+        'Homepage': 'https://tjaart.gitlab.io/python-timew',
+        'Bug Tracker': 'https://gitlab.com/tjaart/python-timew/issues',
+        'Documentation': 'https://gitlab.com/tjaart/python-timew',
+        'Source Code': 'https://gitlab.com/tjaart/python-timew'
+    },
     packages=find_packages('timew'),
-    long_description_markdown_filename='README.md'
+    classifiers=[
+        'Development Status :: 4 - Alpha',
+        'Environment:: Console',
+        'Intended Audience:: Developers',
+        'Operating System:: POSIX',
+        'Programming Language :: Python :: 3'],
+    keywords='timewarrior',
+    license='MIT'
 )
