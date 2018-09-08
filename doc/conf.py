7e79c8cade5d9f3159f86b887b1fe197fcc129fa
Tjaart van der Walt <git@tjaart.org>
2018-09-07 19:10:47 -0700

    Create API documentation

---
+++ doc/conf.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# project = 'Timewarrior API'
+# copyright = '2018, Tjaart van der Walt'
+# author = 'Tjaart van der Walt'
+
+# # The short X.Y version
+# version = ''
+# # The full version, including alpha/beta/rc tags
+# release = '1.0.0'
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinxcontrib.apidoc',
+]
+templates_path = ['_templates']
+source_suffix = ['.rst', '.md']
+master_doc = 'index'
+language = None
+exclude_patterns = []
+pygments_style = 'sphinx'
+html_theme = "classic"
+html_static_path = ['_static']
+htmlhelp_basename = 'TimewarriorAPIdoc'
+todo_include_todos = True
+
+apidoc_module_dir = '../timew'
+apidoc_output_dir = 'src'
+apidoc_excluded_paths = ['tests']
+apidoc_separate_modules = True
