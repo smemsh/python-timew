4ab8d7df35197112be1752d8f501b27202013e6b
Peter Steinbach <steinbach@scionics.de>
2019-01-23 13:37:37 +0100

    fixed python style errors returned by tox and refactored binary path of timew

--- tests/timewarrior_test.py
+++ tests/timewarrior_test.py
@@ -4,6 +4,8 @@ from pytest import fixture
 
 from timew import Duration, TimeWarrior
 
+DEFAULT_BINARY = 'timew'
+
 
 @fixture(scope="module")
 def timew():
@@ -11,70 +13,72 @@ def timew():
 
 
 def test_cancel(timew):
-    assert timew.cancel() == '/usr/bin/timew cancel'
+    assert timew.cancel() == f'{DEFAULT_BINARY} cancel'
 
 
 def test_cont(timew):
-    assert timew.cont(2) == '/usr/bin/timew continue @2'
+    assert timew.cont(2) == f'{DEFAULT_BINARY} continue @2'
 
 
 def test_delete(timew):
-    assert timew.delete(3) == '/usr/bin/timew delete @3'
+    assert timew.delete(3) == f'{DEFAULT_BINARY} delete @3'
 
 
 def test_join(timew):
-    assert timew.join(1, 2) == '/usr/bin/timew join @1 @2'
+    assert timew.join(1, 2) == f'{DEFAULT_BINARY} join @1 @2'
 
 
 def test_lengthen(timew):
     assert timew.lengthen(1, Duration(timedelta(minutes=30))) \
-        == '/usr/bin/timew lengthen @1 PT1800S'
+        == f'{DEFAULT_BINARY} lengthen @1 PT1800S'
+
 
 def test_list(timew):
-    assert type(timew.list()) != None
+    assert not isinstance(timew.list(), type(None))
     assert len(timew.list().split()) == 6
     assert "from" in timew.list()
 
+
 def test_move(timew):
     assert timew.move(5, datetime(2018, 8, 15, 9, 0, 0)) \
-        == '/usr/bin/timew move @5 20180815T090000'
+        == f'{DEFAULT_BINARY} move @5 20180815T090000'
 
 
 def test_shorten(timew):
     assert timew.shorten(2, Duration(timedelta(minutes=10))) \
-        == '/usr/bin/timew shorten @2 PT600S'
+        == f'{DEFAULT_BINARY} shorten @2 PT600S'
 
 
 def test_split(timew):
-    assert timew.split(7) == '/usr/bin/timew split @7'
+    assert timew.split(7) == f'{DEFAULT_BINARY} split @7'
 
 
 def test_start(timew):
     assert timew.start(time=datetime(2018, 8, 15, 9, 0, 0), tags=['my tag']) \
-        == '/usr/bin/timew start 20180815T090000 "my tag"'
+        == f'{DEFAULT_BINARY} start 20180815T090000 "my tag"'
 
 
 def test_stop(timew):
-    assert timew.stop() == '/usr/bin/timew stop'
+    assert timew.stop() == f'{DEFAULT_BINARY} stop'
     assert timew.stop(tags=['my tag1', 'my tag2']) \
-        == '/usr/bin/timew stop "my tag1" "my tag2"'
+        == f'{DEFAULT_BINARY} stop "my tag1" "my tag2"'
 
 
 def test_tag(timew):
     assert timew.tag(5, ['new tag', 'another tag']) \
-        == '/usr/bin/timew tag @5 "new tag" "another tag"'
+        == f'{DEFAULT_BINARY} tag @5 "new tag" "another tag"'
 
 
 def test_untag(timew):
     assert timew.tag(1, ['new tag', 'another tag']) \
-        == '/usr/bin/timew tag @1 "new tag" "another tag"'
+        == f'{DEFAULT_BINARY} tag @1 "new tag" "another tag"'
 
 
 def test_track(timew):
     assert timew.track(start_time=datetime(2018, 8, 15, 9, 0, 0),
                        end_time=datetime(2018, 8, 15, 10, 0, 0)) \
-        == '/usr/bin/timew track from 20180815T090000 - 20180815T100000'
+        == f'{DEFAULT_BINARY} track from 20180815T090000 - 20180815T100000'
     assert timew.track(start_time=datetime(2018, 8, 15, 9, 0, 0),
                        end_time=datetime(2018, 8, 15, 9, 30, 0),
                        tags=['tag 1', 'tag 2']) \
-        == '/usr/bin/timew track from 20180815T090000 - 20180815T093000 "tag 1" "tag 2"'
+        == f'{DEFAULT_BINARY} track from 20180815T090000 - 20180815T093000 "tag 1" "tag 2"'
