54cf5fd2983c3524ac6baca97afe2e7b6708aea3
Tjaart van der Walt <gitlab@tjaart.co.za>
2019-03-05 05:11:44 +0000

    Resolve "Apply Black code formatting"

--- timew/duration.py
+++ timew/duration.py
@@ -10,13 +10,13 @@ class Duration:
 
     def __init__(self, delta):
         if not type(delta) is timedelta:
-            raise DurationError('Duration must be of type datetime.timedelta')
+            raise DurationError("Duration must be of type datetime.timedelta")
         if delta < timedelta(0):
-            raise DurationError('Duration cannot be negative')
+            raise DurationError("Duration cannot be negative")
         self.__delta = delta
 
     def __repr__(self):
-        return 'PT%dS' % self.__delta.total_seconds()
+        return "PT%dS" % self.__delta.total_seconds()
 
     def __str__(self):
-        return 'PT%dS' % self.__delta.total_seconds()
+        return "PT%dS" % self.__delta.total_seconds()
